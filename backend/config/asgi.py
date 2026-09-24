ab28a8fa68f570c133676bb2c3819f2730718e3e
Franco Rodrigo Miranda <167719225+MirandaFrancoCBA@users.noreply.github.com>
2026-09-24 14:25:37 -0300

    Fix Daphne ASGI startup order (#56)
    * Initialize Django before WebSocket routes
    
    * Validate Daphne ASGI startup in CI

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -5,9 +5,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from tasks.routing import websocket_urlpatterns
-
 django_asgi_application = get_asgi_application()
+
+from tasks.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_application,
